d8d66e48cc3ec3d4f35c696d8a2bb5ff714033b3
master <master@SMLAB408PC12_23>
2022-12-13 19:28:22 -0500

    Agregar archivos de proyecto.

---
+++ Semana3JE/Login.xaml.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace Semana3JE
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class Login : ContentPage
+    {
+        public Login()
+        {
+            InitializeComponent();
+        }
+
+        private void btnIniciar_Clicked(object sender, EventArgs e)
+        {
+            string user = "jestrella";
+            string psw = "12345";
+            if (user == txtUser.Text && psw == txtPsw.Text)
+            {
+                Navigation.PushAsync(new Registro(user));
+
+            }
+            else
+            {
+                DisplayAlert("Alerta", "User/Password Incorrect", "Cerrar");
+            }
+
+        }
+    }
+}
